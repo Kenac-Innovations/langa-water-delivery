7d857a35d693c90b5e499b8ec66023dee769bd9f
dylandzvene <dyland@kenac.co.zw>
2025-06-10 13:02:22 +0200

    LWD-2 initial dylan commit

--- take-u-backend/src/main/java/zw/co/kenac/takeu/backend/constant/AppConstant.java
+++ take-u-backend/src/main/java/zw/co/kenac/takeu/backend/constant/AppConstant.java
@@ -6,7 +6,7 @@ public class AppConstant {
     public static final String DELIVERY_REQUEST="Delivery request transaction creation";
     public static final String REQUEST_SIZE_EX="The request size exceeds the maximum limit";
     public static final String CHARGE_TXN="Transaction Charge";
-    public static final String TXN_DEBIT="Transaction Debit";
+    public static final String TXN_DEBIT="Transaction Debit";//
     public static final String TXN_CREDIT="Transaction Credit";
     public static final String TXN_REVERSAL="Transaction Reversal";
 }
